42ad4c2b3fb14e1f8745f5e92ad88a78449e43e7
WorkingPiyush <itsmepiyush855@gmail.com>
2025-07-03 23:56:29 +0530

    Add file compression and JSON storage for uploads

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const uniqueNameing = require('./utils/uniqueNameing.js')
+const uniqueNameing = require('./utils/uniqueNaming.js')
 const fs = require('fs');
 const formidable = require('formidable');
 const http = require('http');
 const path = require('path');
-let port = 7000;
+const zlib = require('zlib')
+const port = 7000;
 
 
 // Create the HTTP server
@@ -33,31 +34,54 @@ const server = http.createServer((req, res) => {
         if (!file || !file.originalFilename) {
           return;
         }
-        const uniqueName = uniqueNameing(file); // Generate a unique filename using crypto for secure storage
+        const tempPath = file.filepath;
+        // const fileName = file.originalFilename
+        const gzipPath = tempPath + '.gz';
 
-        if (!uniqueName) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Failed to genrate the file name !!');
-          return;
-        }
+        const readFile = fs.createReadStream(tempPath);
+        const writeFile = fs.createWriteStream(gzipPath);
+        const gzip = zlib.createGzip();
 
-        let oldPath = file.filepath; // Temporary file path
-        let newPath = path.join(__dirname, 'uploads', uniqueName);// Destination path with its unique name.
-        fs.rename(oldPath, newPath, (err) => { // Move file from temporary path to the final uploads folder
-          if (err) {
+        readFile.pipe(gzip).pipe(writeFile).on('finish', () => {
+          const rawName = uniqueNameing(file) // Generate a unique filename using crypto for secure storage
+          const uniqueName = path.basename(rawName) + `.gz`
+          if (!uniqueName) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error saving the file');
+            res.end('Failed to genrate the file name !!');
             return;
           }
-
-          count++;
-          if (count === uploadedFiles.length) {
-            res.writeHead(200, { 'content-type': 'text/html' });
-            res.end('File Uploaded and Shifted!');
-          }
-        })
+          let newPath = path.join(__dirname, 'uploads', uniqueName);// Destination path with its unique name.
+          fs.rename(gzipPath, newPath, (err) => { // Move file from temporary path to the final uploads folder
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Error saving the file');
+              return;
+            }
+            fs.unlink(tempPath, () => { }); // Removing the temporary files from the temporary storage.
+            // Adding a file named fileData.json to store compressed files and their original names.
+            const metaDataInfo = path.join(__dirname, 'uploads', 'fileData.json');
+            let data = {};
+            if (fs.existsSync(metaDataInfo)) {
+              try {
+                data = JSON.parse(fs.readFileSync(metaDataInfo, 'utf-8'))
+              } catch (error) {
+                console.error("Error while parsing the json file", error.message);
+                data = {};
+              }
+            }
+            // 'aa6cbdffdbef36f9fe2ee7ad494c7bbe1c8ed384f08dc1d25c3680454f0bde37.pdf.gz': 'Software_Engineer_Core_Roadmap.pdf', JSON Representation
+            data[uniqueName] = file.originalFilename;
+            fs.writeFileSync(metaDataInfo, JSON.stringify(data, null, 2));
+            // console.log(fileData) Printing the json  data by importing the file 
+            count++;
+            if (count === uploadedFiles.length) {
+              res.writeHead(200, { 'content-type': 'text/html' });
+              res.end('<h3>File uploaded and compressed successfully!</h3>');
+            }
+          })
+        });
       });
-    });
+    })
   } else {
     // Serve the HTML form when not uploading
     res.writeHead(200, { 'Content-type': 'text/html' });
